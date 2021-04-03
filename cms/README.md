afca5f1219dc79b83bcdbe6f3d51ed3eadbdf354
JonasLeonhard <jonasleonhardfl@gmail.com>
2021-04-03 19:08:47 +0200

    feat(frontend, cms, backend): added frontend, backend, cms
    - added strapi cms with basic article for testing
    - added strapi user roles & permissions
    - added db env setup
    - added docker-compose file
    - added empty server.js in backend
    - added frontend apollo
    - added styled components & helper functions
    - added next-seo
    - added apollo cache inmemory

---
+++ cms/README.md
@@ -0,0 +1,11 @@
+# Strapi application
+
+```bash
+    yarn develop
+```
+
+# Graphql permission error:
+
+go to http://localhost:1337/admin/settings/users-permissions/roles/2
+
+set count, find, findOne
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import GlobalStyle from '../styles/globals.style';
+import theme from '../styles/theme';
+
+import { ThemeProvider } from 'styled-components';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
