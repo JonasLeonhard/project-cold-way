dff155526a6a4c52229d066f3e7c4410e82341d0
JonasLeonhard <jonasleonhardfl@gmail.com>
2021-06-12 02:40:51 +0200

    fix([uuid].tsx): refactored styling to own file

---
+++ frontend/styles/[uuid].style.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import { color, space } from './helper';
+
+const StyledUuidPage = styled.div`
+    display: grid;
+    grid-template-columns: 3fr 25rem;
+    grid-template-rows: 1fr 2rem;
+    width: 100vw;
+    height: calc(100vh - var(--header-height));
+    overflow: hidden;
+
+    .uuidPage__main,
+    .uuidPage__options,
+    .uuidPage__sub-options {
+        overflow: scroll;
+    }
+
+    .uuidPage__options {
+        border-left: 1px solid ${color('grey', 50)};
+    }
+    .uuidPage__sub-options {
+        grid-column: 1 / span 2;
+        box-shadow: 0 2px 8px ${color('shadow')};
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        .uuidPage__sub-options-left {
+            width: 10rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .uuidPage__sub-options-right {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: ${space('s')}
+        }
+    }
+    .uuidPage__tabs {
+        height: 100%;
+
+        .ant-tabs-content {
+            height: 100%;
+        }
+
+        .ant-tabs-nav-list {
+            padding: 0 ${space('m')};
+        }
+    }
+
+    .uuidPage__icon {
+        cursor: pointer;
+    }
+    .uuidPage__icon:hover {
+        filter: brightness(90%);
+    }
+`;
+
+export default StyledUuidPage;
